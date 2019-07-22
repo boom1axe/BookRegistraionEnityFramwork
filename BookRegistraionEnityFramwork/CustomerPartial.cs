0ed960efb8a98453013433bc616f769a6aaa14eb
Dennis Kafton <boom1axe@att.net>
2019-07-22 14:47:43 -0700

    Added a partial customer class to add FullName property

---
+++ BookRegistraionEnityFramwork/CustomerPartial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistraionEnityFramwork
+{
+    public partial class Customer
+    {
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
