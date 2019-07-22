67eee4c30a9227db4178dc710d6c305c87589d6d
Dennis Kafton <boom1axe@att.net>
2019-07-22 14:48:11 -0700

    Populate customer list on form load

--- BookRegistraionEnityFramwork/Form1.cs
+++ BookRegistraionEnityFramwork/Form1.cs
@@ -16,5 +16,21 @@ namespace BookRegistraionEnityFramwork
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateCustomerList();
+        }
+
+        /// <summary>
+        /// Populates customer list from the database
+        /// </summary>
+        private void PopulateCustomerList()
+        {
+            List<Customer> customers = CustomerDB.GetCustomers();
+
+            cboCustomers.DataSource = customers;
+            cboCustomers.DisplayMember = nameof(Customer.FullName);
+        }
     }
 }
